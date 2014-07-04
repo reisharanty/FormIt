11e8e189b6e41c643f843a3ff7dde3f698c77bd5
reisharanty <reisharanty@rocketmail.com>
2014-07-04 11:23:15 +0700

    Add lexicon en in web topic

---
+++ core/components/formit/lexicon/en/web.inc.php
@@ -0,0 +1,5 @@
+<?php
+
+$_lang['formit.product_has_been_updated_successfully'] = 'Product has been updated successfully.';
+$_lang['formit.profile_has_been_updated_successfully'] = 'Profile has been updated successfully.';
+$_lang['formit.profile_has_been_updated_unsuccessfully'] = 'Profile has been updated unsuccessfully.';
